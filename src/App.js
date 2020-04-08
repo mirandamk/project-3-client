4d5643578d7a8eaa57f3de765647704caea5bc27
Emelie Sundgren <emelie.sundgren@hotmail.com>
2020-04-08 12:17:57 +0200

    updating

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: null,
+    };
+  }
+  componentDidMount() {
+    axios
+      .get('http://localhost:3000/home')
+      .then((response) => {
+        debugger;
+        this.setState({ name: response.data });
+      })
+      .catch((err) => {
+        debugger;
+      });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Test page</h1>
+        <>
+          {this.state.name ? (
+            <h1>{this.state.name.name}</h1>
+          ) : (
+            <h1>Loading...</h1>
+          )}
+        </>
+      </div>
+    );
+  }
 }
 
 export default App;
