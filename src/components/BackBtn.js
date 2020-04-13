78a908c599f54bb0d5a1e38028bd3d15ddeff542
Emelie Sundgren <emelie.sundgren@hotmail.com>
2020-04-13 12:40:58 +0200

    getting code

---
+++ src/components/BackBtn.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function BackBtn() {
+    return (
+        <div>
+            <button>
+                <Link to=""/>
+            </button>
+        </div>
+    )
+}
+
+export default BackBtn;
